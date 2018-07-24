57fe7a011eeffa4586e6f417181b4a4f24b11461
MH-Lee <lmhoon012@gmail.com>
2018-07-24 20:18:33 +0900

    nltk_py 수정

--- nltk_py/NLTK_freq.py
+++ nltk_py/NLTK_freq.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[6]:
-
-
 import nltk
 import operator
 from nltk.corpus import stopwords
@@ -19,15 +13,16 @@ from nltk import FreqDist, ngrams
 # nltk.download("webtext")
 # nltk.download("wordnet")
 
-
-# In[2]:
-
-
-test_sentence = "It started before I was born. My biological mother was a young, unwed college graduate student,                and she decided to put me up for adoption. She felt very strongly that I should be adopted by college graduates,                so everything was all set for me to be adopted at birth by a lawyer and his wife.                 Except that when I popped out they decided at the last minute that they really wanted a girl.                 So my parents, who were on a waiting list, got a call in the middle of the night asking: “                We have an unexpected baby boy; do you want him?” They said: “Of course.”                My biological mother later found out that my mother had never graduated from college and that my father had never graduated from high school.                She refused to sign the final adoption papers. She only relented a few months later when my parents promised that I would someday go to college."
-
-
-# In[4]:
-
+test_sentence = "It started before I was born. My biological mother was a young, unwed college graduate student,\
+                and she decided to put me up for adoption. She felt very strongly that I should be adopted by college graduates,\
+                so everything was all set for me to be adopted at birth by a lawyer and his wife.\
+                Except that when I popped out they decided at the last minute that they really wanted a girl.\
+                So my parents, who were on a waiting list, got a call in the middle of the night asking: \
+                “We have an unexpected baby boy; do you want him?” They said: “Of course.”\
+                My biological mother later found out that my mother had never graduated\
+                from college and that my father had never graduated from high school.\
+                She refused to sign the final adoption papers.\
+                She only relented a few months later when my parents promised that I would someday go to college."
 
 def sentence_freq(sentence):
     tokens = nltk.word_tokenize(test_sentence)
@@ -41,9 +36,4 @@ def sentence_freq(sentence):
     word_freq = sorted(dict(freq).items(), key=operator.itemgetter(1), reverse=True)
     return word_freq
 
-
-# In[7]:
-
-
 sentence_freq(test_sentence)
-
