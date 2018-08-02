5ac1bf36d32c11111f76754ed6891e3ec1f05960
1 <ppark9553@gmail.com>
2018-08-02 20:37:44 +0900

    관리자 페이지에서 데이터 정리 편하도록 admin에 모델추가

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Sentence,
+    Text,
+    Word,
+    State,
+    Structure,
+)
+
+admin.site.register(Sentence)
+admin.site.register(Text)
+admin.site.register(Word)
+admin.site.register(State)
+admin.site.register(Structure)
